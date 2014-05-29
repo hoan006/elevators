4f940b45a32735d0d209aff4714159cfe48548f0
Hoan.Nguyen <hoan@misfit.com>
2014-05-30 02:15:26 +0700

    Add secret_token (stupid Github generated .gitignore)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elevators::Application.config.secret_token = 'dd943c0580560fa469aa68eca004fae4ebc7e4b8154a0d599382a1dbec77c3cb7c4064d59f0cc378dbb2c5d45fe24438b7addd6551793e6d34501e298fcde84c'
